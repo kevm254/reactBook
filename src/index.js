8cf89e54d8b2bba5ec09aab12aefc06f8f016daa
kevm254 <kevm2@yahoo.com>
2016-01-26 04:08:04 -0600

    Added link

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+import createHistory from 'history/lib/createHashHistory';
+import List from './pages/List';
 
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-    <Detail message={"This is another message"} />,
+    <Router history={createHistory({ queryKey: false })} onUpdate={() => window.scrollTo(0,0)}>
+        <Route path="/" component={ List } />
+        <Route path="/react" component={ Detail } />
+    </Router>,
     document.getElementById('app')
 );
